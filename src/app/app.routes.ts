3edc7616df70716f4c1a9a5b28ed303ddbe39aaf
AnneQuinkenstein <AnneQuinkenstein@users.noreply.github.com>
2024-03-03 21:07:25 +0100

    UI Changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import {ProjectsComponent} from "./projects/projects.component";
-import {ProjectsUpdateComponent} from "./projects-update/projects-update.component";
 import {RegisterComponent} from "./register/register.component";
 import {MilestoneComponent} from "./milestone/milestone.component";
 import {UsersComponent} from "./users/users.component";
@@ -8,7 +7,6 @@ import {UsersComponent} from "./users/users.component";
 export const routes: Routes = [
   { path: '', component: ProjectsComponent, pathMatch: 'full'},
   { path: 'projects', component: ProjectsComponent, pathMatch: 'full'},
-  { path: 'projects/:id', component: ProjectsUpdateComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'milestone/:id', component: MilestoneComponent },
   { path: 'users', component: UsersComponent, pathMatch: 'full'},
